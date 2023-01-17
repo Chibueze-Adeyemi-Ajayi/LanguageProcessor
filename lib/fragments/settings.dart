8cb4224db2f14c621b1b2144d68c5304d0533f2c
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-01-17 12:52:17 +0530

    Mobile App

--- lib/fragments/settings.dart
+++ lib/fragments/settings.dart
@@ -10,14 +10,39 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
   
+  var languages = [
+    "English", "Spanish", "Chinese", "Japanese"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body:  Container(
+        padding: EdgeInsets.all(10),
+        color: Colors.white,
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         child: SingleChildScrollView(child: Column(children: [
-          
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+            Text("Select input language", textAlign: TextAlign.start,),
+            Container(height: 25,),
+            DropdownButton(
+              value: "English",
+              icon: const Icon(Icons.keyboard_arrow_down),
+              items: languages.map((String items) {
+                return DropdownMenuItem(
+                  value: items,
+                  child: Text(items),
+                );
+              }).toList(), 
+              onChanged: (index) {
+
+            }),
+            Container(height: 25,),
+            Text("Select output language"),
+          ],)
         ],)),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
