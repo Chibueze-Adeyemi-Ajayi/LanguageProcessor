f8c1387a1f70f5ebe70683c52fd63e1a94eec342
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-01-17 08:18:01 +0530

    Mobile App

--- lib/utils/LanguageNetwork.dart
+++ lib/utils/LanguageNetwork.dart
@@ -6,12 +6,14 @@ class LanguageNetwork {
   static final languageIdentifier = LanguageIdentifier(confidenceThreshold: 0.5);
 
   static Future <String> detectLanguage (text, context) async {
-    showDialog(context: context, builder: (context) {
+    showDialog(
+      barrierDismissible: false,
+      context: context, builder: (context) {
       return Center(child: CircularProgressIndicator());
     });
     final response = await languageIdentifier.identifyLanguage(text);
     final List<IdentifiedLanguage> possibleLanguages = await languageIdentifier.identifyPossibleLanguages(text);
-    //Navigator.pop(context);
+    Navigator.pop(context);
     return response;
   }
   
