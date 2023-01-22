34bd1f17531ac03ee131f17789678facf5b2ce9b
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-01-22 03:55:24 +0100

    Web app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
+import Banner from './components/banner';
 
 function App() {
   return (
     <>
       {/* app header */}
       <Header />
+      {/* banner */}
+      <Banner />
     </>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -6,8 +6,16 @@ let Header = () => {
            <img src={icon}></img>
            <font className="my-auto w-96 font-bold text-gray-800 text-2xl">Language Processor</font>
            <section className="w-full grid-cols-3 h-full grid gap-3">
-                <div className="w-full col-span-2 flex flex-row space-x-4">
-
+                <div className="w-full col-span-2 flex">
+                    <section className=" flex flex-row space-x-4 mx-auto">
+                        <a className="w-24 h-fit text-center my-auto transform transition-all duration-250 hover:text-blue-600 hover:font-bold cursor-pointer">Home</a>
+                        <a className="w-24 h-fit text-center my-auto transform transition-all duration-250 hover:text-red-600 hover:font-bold cursor-pointer">About</a>
+                        <a className="w-24 h-fit text-center my-auto transform transition-all duration-250 hover:text-green-600 hover:font-bold cursor-pointer">Contact</a>
+                        <a className="w-24 h-fit text-center my-auto transform transition-all duration-250 hover:text-yellow-600 hover:font-bold cursor-pointer">Developer</a>
+                    </section>
+                </div>
+                <div className="w-full flex">
+                    <button className="px-6 py-2 my-auto mx-auto bg-blue-600 text-white rounded-md">Download</button>
                 </div>
            </section>
         </nav>
