6ff3a7cc247618d5aa536cce920309457325f538
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-01-22 04:05:52 +0100

    Web app

--- src/components/banner.js
+++ src/components/banner.js
@@ -8,17 +8,18 @@ const Banner = () => {
             <h1 className="font-bold text-5xl w-full text-center text-gray-800">Language Processor</h1>
             <font className="text-center font-5xl font-semibold">Any word, any language, you can translate. The future of translation. </font>
             
-            {/* logo section */}
+            {/* logo & text section */}
             <div className="w-full h-[400px] grid grid-cols-2 gap-3">
+                {/* image section */}
                 <section className="w-full p-12 h-full flex">
                     <img className="my-auto" src={logo}/>
                 </section>
-            </div>  
-
-            {/* text section */}
-            <div>
                 
-            </div>
+                {/* text section */}
+                <div className="w-full h-full">
+
+                </div>
+            </div>  
 
         </section>  
     );
