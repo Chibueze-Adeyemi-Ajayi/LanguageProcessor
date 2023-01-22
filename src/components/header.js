a9354835abade32d0e3821136388a4d24b2b3d12
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-01-22 03:41:51 +0100

    Web app

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,14 @@
+import icon from "../assets/img/icon.png";
+
 let Header = () => {
     return (
-        <div>Hola</div>
+        <nav className="w-full p-2 h-16 bg-white border-b flex flex-row space-x-3 border-gray-100">
+           <img src={icon}></img>
+           <font className="my-auto font-bold text-gray-800 text-2xl">Language Processor</font>
+           <section>
+            
+           </section>
+        </nav>
     );
 }
 
