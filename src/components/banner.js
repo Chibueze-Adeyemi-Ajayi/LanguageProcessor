d887b7ceb3c98b3c977c312ebb27f15b423b1c62
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-01-22 04:01:05 +0100

    Web app

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,8 +1,14 @@
+import logo from "../assets/img/logo.png";
+
 const Banner = () => {
     return (
-        <section>
+        <section className="w-full h-fit p-6 flex flex-col space-y-6">
+            <h1 className="font-bold text-5xl w-full text-center text-gray-800">Language Processor</h1>
+            <font className="text-center font-5xl font-semibold">The state of the art in Natural Language Processing. </font>
+            <div className="w-full h-fit grid grid-cols-2 gap-3">
 
-        </section>
+            </div>  
+        </section>  
     );
 }
 
