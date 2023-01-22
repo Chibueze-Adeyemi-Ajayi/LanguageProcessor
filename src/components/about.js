225258b0226a7951023f69ea73c5e5fc0011f79d
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-01-22 04:27:42 +0100

    Web app

--- src/components/about.js
+++ src/components/about.js
@@ -1,8 +1,26 @@
+import logo from "../assets/img/logo.png";
+
+const GridSection = (params) => {
+    return (
+        <section className="w-full h-fit grid grid-cols-2 gap-4">
+            <div className="w-full h-fit p-4 flex"> {params.left} </div>
+            <div className="w-full h-fit p-4 flex"> {params.right} </div>
+        </section>
+    );
+} 
+
+const Image = (params) => {
+    return <img className="mx-auto" src={params.src}></img>
+}
+
 const About = () => {
     return (
-        <section>
-            
-        </section>
+        <div className="w-full h-fit  bg-gray-100 ">
+            <section className="w-full h-fit p-6 flex flex-col space-y-6">
+                <h1 className="w-full h-fit mx-auto text-4xl font-extrabold text-gray-800">About App</h1>
+                <GridSection left={<Image src/>} />
+            </section>
+        </div>
     );
 }
 
