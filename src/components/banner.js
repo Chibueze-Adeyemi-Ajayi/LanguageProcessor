0dca5ec910ad9e50ea00d7bbcf1004374368f161
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-01-22 04:04:55 +0100

    Web app

--- src/components/banner.js
+++ src/components/banner.js
@@ -3,11 +3,23 @@ import logo from "../assets/img/logo.png";
 const Banner = () => {
     return (
         <section className="w-full h-fit p-6 flex flex-col space-y-6">
-            <h1 className="font-bold text-5xl w-full text-center text-gray-800">Language Processor</h1>
-            <font className="text-center font-5xl font-semibold">The state of the art in Natural Language Processing. </font>
-            <div className="w-full h-fit grid grid-cols-2 gap-3">
 
+            {/* heading section */}
+            <h1 className="font-bold text-5xl w-full text-center text-gray-800">Language Processor</h1>
+            <font className="text-center font-5xl font-semibold">Any word, any language, you can translate. The future of translation. </font>
+            
+            {/* logo section */}
+            <div className="w-full h-[400px] grid grid-cols-2 gap-3">
+                <section className="w-full p-12 h-full flex">
+                    <img className="my-auto" src={logo}/>
+                </section>
             </div>  
+
+            {/* text section */}
+            <div>
+                
+            </div>
+
         </section>  
     );
 }
