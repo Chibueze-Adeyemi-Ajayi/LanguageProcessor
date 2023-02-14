5505fa9fcbbcb002e6c2cea400ee8681d8a44c1d
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-14 21:16:40 +0100

    Language Processor APP

--- lib/utils/LocalStorage.dart
+++ lib/utils/LocalStorage.dart
@@ -1,6 +1,6 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-// this module hand
+// this module handl
 
 class LocalStorage {
    LocalStorage({required this.callback});  final callback;
