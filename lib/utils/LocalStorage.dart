2548f1d1f663ce27cede403457525c09c5839603
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-15 22:50:08 +0100

    mobile app

--- lib/utils/LocalStorage.dart
+++ lib/utils/LocalStorage.dart
@@ -1,7 +1,7 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 // this module handles 
-// localstorage stores the user's select
+// localstorage stores the user's selecte
 
 class LocalStorage {
    LocalStorage({required this.callback});  final callback;
