428f65a4c8bae0d19d662b14ed189572ebb95688
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-15 22:55:34 +0100

    mobile app

--- lib/utils/LocalStorage.dart
+++ lib/utils/LocalStorage.dart
@@ -1,7 +1,7 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 // this module handles 
-// localstorage stores the user's selected default language i.e translate and primary languag
+// localstorage stores the user's selected default language i.e translate and primary language
 
 class LocalStorage {
    LocalStorage({required this.callback});  final callback;
