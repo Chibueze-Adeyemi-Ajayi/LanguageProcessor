44a43a65ca3b24be17514ded316a480c6dbd054c
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-18 21:48:11 +0100

    mobile app

--- lib/screens/index.dart
+++ lib/screens/index.dart
@@ -6,7 +6,7 @@ import 'package:language_processor/utils/TextToSpeech.dart';
 import 'package:language_processor/utils/speech.dart';
 
 // this is a language based application that processes natural (spoken words) 
-// google ml kit
+// google ml kit 
 
 class IndexScreen extends StatefulWidget {
   const IndexScreen({Key? key, required this.callback}) : super(key: key);
