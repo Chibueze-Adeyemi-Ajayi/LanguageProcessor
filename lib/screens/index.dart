9ce01a5bbe2e8c14ea5a3c679d73fce87912e46b
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-18 21:42:24 +0100

    mobile app

--- lib/screens/index.dart
+++ lib/screens/index.dart
@@ -5,7 +5,7 @@ import 'package:language_processor/fragments/settings.dart';
 import 'package:language_processor/utils/TextToSpeech.dart';
 import 'package:language_processor/utils/speech.dart';
 
-// this is a language based application that processes natural (s)
+// this is a language based application that processes natural (sp)
 
 class IndexScreen extends StatefulWidget {
   const IndexScreen({Key? key, required this.callback}) : super(key: key);
