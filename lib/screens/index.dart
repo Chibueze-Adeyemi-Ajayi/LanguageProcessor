62f94b78f7c5ab3d9d15310d4429491f91a3e722
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-18 22:03:28 +0100

    mobile app

--- lib/screens/index.dart
+++ lib/screens/index.dart
@@ -9,7 +9,7 @@ import 'package:language_processor/utils/speech.dart';
 // google ml kit being used for natural language processing
 // engine used: google translate
 // language: dart
-
+// 
 
 class IndexScreen extends StatefulWidget {
   const IndexScreen({Key? key, required this.callback}) : super(key: key);
